9dc35723b3ad0bb85df8b2b74ba9001007fc3f72
ZsemberiDaniel <zsemberi.daniel@gmail.com>
2019-10-26 12:04:34 +0200

    Base

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import userRouter from "./routes/users";
+
+const app = express();
+
+app.use(express.static(__dirname + "/src"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/asd", userRouter);
+
+app.listen(3000, () =>
+  console.log(`Server is listening on port ${3000}`)
+);
---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {};
+
+config.admin={};
+
+config.app={};
+
+config.admin.name="Harcos Hörcsögök";
+config.admin.email="";
+config.app.name="TelekomApp";
+config.app.url="";
+config.mode="development";
+
+module.exports=config
