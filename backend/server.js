0081d05a32eedacedb30c742261fa37c88de3a10
Gergely Tarsoly <gergely.tarsoly3@gmail.com>
2019-10-26 18:29:57 +0200

    Modify server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import config from '../config.js';
 import db from "./db_connector.js"
+import Person from '../src/app/data/Person.jsx';
+
+// function to_person(db_row) {
+//   return new Person();
+// }
 
 const router = express.Router();
 
@@ -8,8 +13,11 @@ const db_connector = new db;
 
 router.get('/skills/:skill', function(req, res, next) {
   let skill = req.params.skill;
-  db_connector.query_person_by_skill(skill, val => { res.send(val) });
+  db_connector.query_person_by_skill(skill, val => { console.log(res); res.send(val) });
 });
+
+// team -> [person]
+// technology -> [team]
 
 // router.get('/skills/:skill', function(req, res, next) {
 //   let skill = req.params.skill;
