a00744a39af4a2c12837e7d76bc9f348a3b71785
Gergely Tarsoly <gergely.tarsoly3@gmail.com>
2019-10-26 16:54:42 +0200

    Add db query functionality

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import config from '../config.js';
-import message from "./db_connector.js"
+import db from "./db_connector.js"
 
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
+const db_connector = new db;
+
+router.get('/:skill', function(req, res, next) {
+  let skill = req.params.skill;
+  console.log(skill);
   // GET/users/ route
-  res.send({
-    name: config.admin.name,
-    msg: message});
+  db_connector.query_person_by_skill(skill, val => { res.send(val) });
+  // res.send({
+  //   name: config.admin.name,
+  //   msg: message});
 });
 
 export default router;
