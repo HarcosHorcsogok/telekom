99a52e507dae4fa089c19c3150cd0d2330e43dad
ZsemberiDaniel <zsemberi.daniel@gmail.com>
2019-10-26 18:06:12 +0200

    no message

--- src/app/components/TopAppBar.jsx
+++ src/app/components/TopAppBar.jsx
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
+import Grow from '@material-ui/core/Grow'
+import IconButton from '@material-ui/core/IconButton';
 
 const theme = createMuiTheme({
   palette: {
@@ -22,8 +24,7 @@ const theme = createMuiTheme({
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-    color: "#E10075"
+    flexGrow: 1
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -38,66 +39,70 @@ const useStyles = makeStyles(theme => ({
   search: {
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
+    backgroundColor: fade(theme.palette.common.white, 1),
     marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: '100%',
-    },
+    width: '40%'
   },
   searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+    color: 'white',
     pointerEvents: 'none',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
   inputRoot: {
-    color: 'inherit',
+    color: 'black',
+    width: '100%'
   },
   inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: "100%",
-      '&:focus': {
-        width: "100%",
-      },
-    },
+    padding: theme.spacing(1, 1, 1, 1),
+    width: '100%'
   },
 }));
 
-export default function TopAppBar() {
+/* 
+ * Defines a new top app bar.
+ * :param props Needs a searchCallback function which will be called each time a search term is entered into the field 
+*/
+export default function TopAppBar(props) {
+  const searchCallback = props.searchCallback;
   const classes = useStyles();
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked(prev => !prev);
+  };
 
   return (
     <ThemeProvider theme={theme}>
         <div className={classes.root}>
         <AppBar position="static">
             <Toolbar>
-            <Typography className={classes.title} variant="h6" noWrap>
-                HOL/KI
-            </Typography>
-            <div className={classes.search}>
-                <div className={classes.searchIcon}>
-                <SearchIcon />
+                <Typography className={classes.title} variant="h6" noWrap>
+                    HOL/KI
+                </Typography>
+                <Grow in={checked}>
+                    <div className={classes.search}>
+                        <InputBase
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                            onChange={searchCallback}
+                        />
+                    </div>
+                </Grow>
+                <div class="font-icon-wrapper" onClick={() => handleChange()}>
+                    <IconButton>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                    </IconButton>
                 </div>
-                <InputBase
-                placeholder="Search…"
-                classes={{
-                    root: classes.inputRoot,
-                    input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-                />
-            </div>
             </Toolbar>
         </AppBar>
         </div>
