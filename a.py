763fbcfcc030660442f4d4feb8014a227bcc7026
Gergely Tarsoly <gergely.tarsoly3@gmail.com>
2019-10-27 02:44:22 +0100

    Add new images. Extend database.

---
+++ a.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import os
+
+path = 'c:\\Users\\geri1245\\Desktop\\images\\'
+
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+  for file in f:
+    files.append(os.path.join(r, file))
+
+for f in files:
+  path = f.split("\\")
+  path[4] = "imgs"
+  dst = "\\".join(path)
+
+  basewidth = 300
+  img = Image.open(f)
+  wpercent = (basewidth/float(img.size[0]))
+  hsize = int((float(img.size[1])*float(wpercent)))
+  img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+  img.save(dst)
